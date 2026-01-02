65ccada87c710c26f860a1ee5ad0ab38e2fee5c1
Piyush Garg <125341290+Amanassert@users.noreply.github.com>
2026-01-02 22:49:29 +0530

    new update

--- index.js
+++ index.js
@@ -23,6 +23,40 @@ app.get("/download-apk", (req, res) => {
   fileStream.pipe(res);
 });
 
+app.get("/helix-jump-stack-ball-3D-game-apk", (req, res) => {
+  const apkPath = path.join(__dirname, "apks", "helixjump.apk");
+
+  if (!fs.existsSync(apkPath)) {
+    return res.status(404).send("APK not found in Current Directory");
+  }
+
+  res.setHeader("Content-Type", "application/vnd.android.package-archive");
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=HelixJump.apk"
+  );
+
+  const fileStream = fs.createReadStream(apkPath);
+  fileStream.pipe(res);
+});
+
+app.get("/Ben&Bart", (req, res) => {
+  const apkPath = path.join(__dirname, "apks", "helixjump.apk");
+
+  if (!fs.existsSync(apkPath)) {
+    return res.status(404).send("APK not found in Current Directory");
+  }
+
+  res.setHeader("Content-Type", "application/vnd.android.package-archive");
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=HelixJump.apk"
+  );
+
+  const fileStream = fs.createReadStream(apkPath);
+  fileStream.pipe(res);
+});
+
 // Server start
 app.listen(PORT, () => {
   console.log(`APK Server running on http://localhost:${PORT}`);
